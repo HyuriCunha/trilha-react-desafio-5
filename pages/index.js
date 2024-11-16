54e65752c645e513f612d4353952bf7013e270c1
HyuriCunha <hyuri.hgc@hotmail.com>
2024-11-16 15:39:04 -0300

    algumas mudanças

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,7 @@ export default function Index({ posts, globalData }) {
               <Link
                 as={`/posts/${post.id}`}
                 href={`/posts/${post.id}`}
-              >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
                   {post.created_ate && (
                     <p className="uppercase mb-3 font-bold opacity-60">
                       {post.created_at}
@@ -40,7 +39,7 @@ export default function Index({ posts, globalData }) {
                     </p>
                   )}
                   <ArrowIcon className="mt-4" />
-                </a>
+               
               </Link>
             </li>
           ))}
--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://adflmboaqqvopezqeqmw.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZmxtYm9hcXF2b3BlenFlcW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3Nzc5MzIsImV4cCI6MjA0NzM1MzkzMn0.9GsWLiZn7ug_i_yKxBgzXycb5CwBZzn2yBy3GmJmINU",
+        authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZmxtYm9hcXF2b3BlenFlcW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3Nzc5MzIsImV4cCI6MjA0NzM1MzkzMn0.9GsWLiZn7ug_i_yKxBgzXycb5CwBZzn2yBy3GmJmINU"
     }
 })
